a0e44714fb752e6d31b19a6f775a6fc8e1ff2c61
Anibal Andrade <59257555+androso@users.noreply.github.com>
2023-11-21 02:27:29 +0000

    moving to local

---
+++ js/graph.js
@@ -0,0 +1,29 @@
+// const graph = document.getElementById('comparisonAlgorithmsGraph');
+
+// const bubbleSortData = {
+//     x: [1000, 2000, 3000, 4000],
+//     y: [12, 9, 15, 12],
+//     type: 'scatter',
+//     name: 'bubbleSortData'
+// };
+
+// const insertionSortData = {
+//     x: [1000, 2000, 3000, 4000],
+//     y: [16, 5, 11, 9],
+//     type: 'scatter',
+//     name: 'insertionSortData'
+// };
+
+// const data = [bubbleSortData, insertionSortData];
+
+// const layout = {
+//     title: "Comparando Algoritmos de Ordenamiento",
+//     xaxis: {
+//         title: 'Datos (Numero de Datos usados para el ordenamiento)',
+//     },
+//     yaxis: {
+//         title: 'Tiempo (Milisegundos)',
+//     }
+// }
+
+// Plotly.newPlot(graph, data, layout);
--- main.js
+++ main.js
@@ -1,45 +1,26 @@
+// require('dotenv').config();
 
+// const API_KEY = process.env.API_KEY;
+// const algos = require('./algorithms');
 
-require('dotenv').config();
+// const getDuration = (sortingFn, listado) => {
+//     const startTime = performance.now();
+//     const result = sortingFn(listado);
+//     const endTime = performance.now();
 
-const API_KEY = process.env.API_KEY;
-const algos = require('./algorithms');
+//     return `${(endTime - startTime).toFixed(3)}ms`;
+// }
 
-const getDuration = (sortingFn, listado) => {
-    const startTime = performance.now();
-    const result = sortingFn(listado);
-    const endTime = performance.now();
+const startBenchmark = async () => {
+    console.log("hey")
+    let primerLote = []; // 100 peliculas
+    let segundoLote = []; // 500 peliculas
+    let tercerLote = []; // 1000 peliculas
+    let cuartoLote = []; // 1500 peliculas
 
-    return `${(endTime - startTime).toFixed(3)}ms`;
-}
-
-(async () => {
-    let primerLote = []; // 20 peliculas
-    let segundoLote = []; // 60 peliculas
-    let tercerLote = []; // 100 peliculas
-    let cuartoLote = []; // 200 peliculas
-    const allMoviesTitles = [];
-    const urls = [
-        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${API_KEY}`,
-        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=2&api_key=${API_KEY}`,
-        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=3&api_key=${API_KEY}`,
-        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=4&api_key=${API_KEY}`,
-        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=5&api_key=${API_KEY}`,
-        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=6&api_key=${API_KEY}`,
-        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=7&api_key=${API_KEY}`,
-        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=8&api_key=${API_KEY}`,
-        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=9&api_key=${API_KEY}`,
-        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=10&api_key=${API_KEY}`
-    ];
-
-
-    for (let i = 0; i < urls.length; i++) {
-        const url = urls[i];
-        const response = await fetch(url);
-        const data = await response.json();
-        allMoviesTitles.push(...data.results.map((pelicula) => pelicula.title));
-    }
-
+    const allMoviesJson = await (await fetch("./listMovies.json")).json();
+    console.log({ allMoviesJson });
+    return;
     primerLote = allMoviesTitles.slice(0, 20);
     segundoLote = allMoviesTitles.slice(0, 60);
     tercerLote = allMoviesTitles.slice(0, 100);
@@ -74,4 +55,11 @@ const getDuration = (sortingFn, listado) => {
     })
 
     console.log(resultados);
-})();
+}
+
+document.getElementById('startBenchmark').addEventListener("click", () => {
+    console.log("?")
+    // const data = await startBenchmark();
+    // console.log({ data });
+})
+
